eca5d24f021df384a29dae1435bc3a1665772f88
Yujin <146316031+yjulianlp@users.noreply.github.com>
2024-10-23 00:20:53 -0600

    feat: add event name to event class

--- app/src/main/java/com/example/eventapp/Event.java
+++ app/src/main/java/com/example/eventapp/Event.java
@@ -1,4 +1,17 @@
 package com.example.eventapp;
 
 public class Event {
+    private String eventName;
+
+    public Event(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
 }
