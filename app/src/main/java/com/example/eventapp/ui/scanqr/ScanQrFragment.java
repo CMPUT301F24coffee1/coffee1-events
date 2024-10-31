961355c12ab8437555ce1a3828b026518f67c825
Maro Erivona <maroerivona@gmail.com>
2024-10-31 16:25:10 -0600

    refactor: remove unused import statements

--- app/src/main/java/com/example/eventapp/ui/scanqr/ScanQrFragment.java
+++ app/src/main/java/com/example/eventapp/ui/scanqr/ScanQrFragment.java
@@ -1,12 +1,9 @@
 package com.example.eventapp.ui.scanqr;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -29,8 +26,8 @@ public class ScanQrFragment extends Fragment {
                 if (result.getContents() != null) {
                     // Display the scanned result in the TextView
                     binding.textScanQr.setText("Scanned: " + result.getContents());
-                } else {
-                    binding.textScanQr.setText("Scan Cancelled");
+                } else { // Else toast "Scan cancelled"
+                    Toast.makeText(getContext(), "Scan Cancelled", Toast.LENGTH_SHORT).show();
                 }
             });
 
@@ -42,14 +39,9 @@ public class ScanQrFragment extends Fragment {
         binding = FragmentScanQrBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Set up the scan button click listener
         binding.buttonScanQr.setOnClickListener(v -> initiateQrScan());
 
         return root;
-
-//        final TextView textView = binding.textScanQr;
-//        scanQrViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//        return root;
     }
 
     private void initiateQrScan() {
@@ -58,7 +50,7 @@ public class ScanQrFragment extends Fragment {
         options.setOrientationLocked(true);
         options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);  // Restrict to QR codes only
 
-        scanLauncher.launch(options);  // Launch the scan
+        scanLauncher.launch(options);
     }
 
     @Override
