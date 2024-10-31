ac89c7f62d4ca045c063a937cc23a171e28def0b
Maro Erivona <maroerivona@gmail.com>
2024-10-31 17:33:10 -0600

    refactor: remove unused import statements

--- app/src/main/java/com/example/eventapp/ui/scanqr/ScanQrFragment.java
+++ app/src/main/java/com/example/eventapp/ui/scanqr/ScanQrFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
