80f8e5ec2b98f641ebd15545d85e609dd57e0516
Greg K-M <gkubiski@hotmail.com>
2024-11-08 06:56:29 -0700

    add profiles test folder

---
+++ app/src/androidTest/java/com/example/eventapp/ui/profiles/AdminProfilesFragmentTest.java
@@ -0,0 +1,4 @@
+package com.example.eventapp.ui.profiles;
+
+public class AdminProfilesFragmentTest {
+}
--- app/src/test/java/com/example/eventapp/ui/profiles/AdminProfilesFragmentTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventapp.ui.profiles;
-
-import junit.framework.TestCase;
-
-public class AdminProfilesFragmentTest extends TestCase {
-
-}
