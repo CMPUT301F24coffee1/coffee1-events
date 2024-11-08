2047cd666c2f7eb6c80d9c6c4356d0c8134584a4
Greg K-M <gkubiski@hotmail.com>
2024-11-08 14:39:07 -0700

    add simple ui test, fix main activity layout button access issue

--- app/src/androidTest/java/com/example/eventapp/ui/events/EventsFragmentTest.java
+++ app/src/androidTest/java/com/example/eventapp/ui/events/EventsFragmentTest.java
@@ -1,4 +1,39 @@
 package com.example.eventapp.ui.events;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.eventapp.MainActivity;
+import com.example.eventapp.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class EventsFragmentTest {
+
+    @Before
+    public void setUp() {
+        // Launch MainActivity where EventsFragment is hosted
+        ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void testShowCreateEventPopup() {
+        // Click on the create event button
+        onView(withId(R.id.create_event_button)).perform(click());
+
+        // Check if the CreateEventFragment is displayed by looking for a unique element in it
+        onView(withId(R.id.popup_create_event_button)).check(matches(isDisplayed()));
+    }
+
 }
