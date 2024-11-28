cadfefa143338c14af390297102fcb45f2db7743
jake158 <kulikovpavel2801@gmail.com>
2024-11-28 13:47:24 -0700

    feat: add handling organizer status change, facility removal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,95 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import * as logger from 'firebase-functions/logger';
+import {
+  onDocumentUpdated,
+  onDocumentDeleted,
+} from 'firebase-functions/v2/firestore';
+import { User } from './types/user';
+import { deleteDocumentsByQuery } from './utils';
+
+initializeApp();
+const db = getFirestore();
+
 /**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * When a user removes their organizer status
+ * Remove all of their associated facilities.
  */
+export const handleOrganizerStatusChange = onDocumentUpdated(
+  'users/{userId}',
+  async (event) => {
+    const snapshot = event.data;
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+    if (!snapshot) {
+      logger.warn('No data associated with the event');
+      return;
+    }
+    const dataBefore = snapshot.before.data() as User;
+    const dataAfter = snapshot.after.data() as User;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+    if (!(dataBefore?.organizer === true && dataAfter?.organizer === false)) {
+      return;
+    }
+    const organizerId = dataBefore.userId;
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+    logger.debug(
+      `User ${organizerId} is no longer an organizer. Deleting facilities...`
+    );
+
+    try {
+      const facilitiesQuery = db
+        .collection('facilities')
+        .where('organizerId', '==', organizerId);
+
+      return deleteDocumentsByQuery(facilitiesQuery).then((deletedCount) => {
+        logger.debug(
+          `Deleted ${deletedCount} facilities for user ${organizerId}.`
+        );
+      });
+    } catch (error) {
+      logger.error(
+        `Failed to delete facilities for user ${organizerId}:`,
+        error
+      );
+    }
+  }
+);
+
+/**
+ * When a facility is removed
+ * Remove all events associated with that facility.
+ */
+export const handleFacilityDeleted = onDocumentDeleted(
+  'facilities/{facilityId}',
+  async (event) => {
+    const snapshot = event.data;
+
+    if (!snapshot) {
+      logger.warn('No data associated with the event');
+      return;
+    }
+
+    const facilityId = event.params.facilityId;
+
+    logger.debug(
+      `Facility ${facilityId} deleted. Finding associated events...`
+    );
+
+    try {
+      const eventsQuery = db
+        .collection('events')
+        .where('facilityId', '==', facilityId);
+
+      return deleteDocumentsByQuery(eventsQuery).then((deletedCount) => {
+        logger.debug(
+          `Deleted ${deletedCount} events for facility ${facilityId}.`
+        );
+      });
+    } catch (error) {
+      logger.error(
+        `Failed to delete events for facility ${facilityId}:`,
+        error
+      );
+    }
+  }
+);
