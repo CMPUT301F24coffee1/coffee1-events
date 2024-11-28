23722747d6afcd05d73a594a7ab9f9282f4677c8
jake158 <kulikovpavel2801@gmail.com>
2024-11-28 16:17:57 -0700

    feat: add handling for deleted users, events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,7 @@ const db = getFirestore();
 
 /**
  * When a user removes their organizer status
- * Remove all of their associated facilities.
+ * Delete all of their associated facilities.
  */
 export const handleOrganizerStatusChange = onDocumentUpdated(
   'users/{userId}',
@@ -30,7 +30,7 @@ export const handleOrganizerStatusChange = onDocumentUpdated(
     if (!(dataBefore?.organizer === true && dataAfter?.organizer === false)) {
       return;
     }
-    const organizerId = dataBefore.userId;
+    const organizerId = event.params.userId;
 
     logger.debug(
       `User ${organizerId} is no longer an organizer. Deleting facilities...`
@@ -41,11 +41,10 @@ export const handleOrganizerStatusChange = onDocumentUpdated(
         .collection('facilities')
         .where('organizerId', '==', organizerId);
 
-      return deleteDocumentsByQuery(facilitiesQuery).then((deletedCount) => {
-        logger.debug(
-          `Deleted ${deletedCount} facilities for user ${organizerId}.`
-        );
-      });
+      const deletedCount = await deleteDocumentsByQuery(facilitiesQuery);
+      logger.debug(
+        `Deleted ${deletedCount} facilities for user ${organizerId}.`
+      );
     } catch (error) {
       logger.error(
         `Failed to delete facilities for user ${organizerId}:`,
@@ -56,19 +55,49 @@ export const handleOrganizerStatusChange = onDocumentUpdated(
 );
 
 /**
- * When a facility is removed
- * Remove all events associated with that facility.
+ * When a user is deleted
+ * Delete all signups and facilities associated with the user.
+ */
+export const handleUserDeleted = onDocumentDeleted(
+  'users/{userId}',
+  async (event) => {
+    const userId = event.params.userId;
+
+    logger.debug(
+      `User ${userId} is being deleted. Deleting facilities and signups...`
+    );
+
+    const queries = [
+      {
+        name: 'facilities',
+        query: db.collection('facilities').where('organizerId', '==', userId),
+      },
+      {
+        name: 'signups',
+        query: db.collection('signups').where('userId', '==', userId),
+      },
+    ];
+
+    try {
+      for (const { name, query } of queries) {
+        const deletedCount = await deleteDocumentsByQuery(query);
+        logger.debug(
+          `Deleted ${deletedCount} ${name} documents for user ${userId}.`
+        );
+      }
+    } catch (error) {
+      logger.error(`Failed to delete documents for user ${userId}:`, error);
+    }
+  }
+);
+
+/**
+ * When a facility is deleted
+ * Delete all events associated with that facility.
  */
 export const handleFacilityDeleted = onDocumentDeleted(
   'facilities/{facilityId}',
   async (event) => {
-    const snapshot = event.data;
-
-    if (!snapshot) {
-      logger.warn('No data associated with the event');
-      return;
-    }
-
     const facilityId = event.params.facilityId;
 
     logger.debug(
@@ -80,16 +109,39 @@ export const handleFacilityDeleted = onDocumentDeleted(
         .collection('events')
         .where('facilityId', '==', facilityId);
 
-      return deleteDocumentsByQuery(eventsQuery).then((deletedCount) => {
-        logger.debug(
-          `Deleted ${deletedCount} events for facility ${facilityId}.`
-        );
-      });
+      const deletedCount = await deleteDocumentsByQuery(eventsQuery);
+      logger.debug(
+        `Deleted ${deletedCount} events for facility ${facilityId}.`
+      );
     } catch (error) {
       logger.error(
         `Failed to delete events for facility ${facilityId}:`,
         error
       );
+    }
+  }
+);
+
+/**
+ * When an event is deleted
+ * Delete all signups associated with that event.
+ */
+export const handleEventDeleted = onDocumentDeleted(
+  'events/{eventId}',
+  async (event) => {
+    const eventId = event.params.eventId;
+
+    logger.debug(`Facility ${eventId} deleted. Deleting all signups...`);
+
+    try {
+      const signupsQuery = db
+        .collection('signups')
+        .where('eventId', '==', eventId);
+
+      const deletedCount = await deleteDocumentsByQuery(signupsQuery);
+      logger.debug(`Deleted ${deletedCount} signups for event ${eventId}.`);
+    } catch (error) {
+      logger.error(`Failed to delete signups for event ${eventId}:`, error);
     }
   }
 );
