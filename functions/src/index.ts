6c1ec55fd2a5bb41a05f4a115ccc3d0650c0fd1b
jake158 <kulikovpavel2801@gmail.com>
2024-12-02 10:39:21 -0700

    feat: add full lottery processing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
+import { onTaskDispatched } from 'firebase-functions/v2/tasks';
+import { getFunctions } from 'firebase-admin/functions';
 import { onCall, HttpsError } from 'firebase-functions/v2/https';
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import * as logger from 'firebase-functions/logger';
 import {
+  onDocumentCreated,
   onDocumentUpdated,
   onDocumentDeleted,
 } from 'firebase-functions/v2/firestore';
@@ -15,7 +18,7 @@ initializeApp();
 const db = getFirestore();
 
 /**
- * Processes an event lottery when called from the app by an organizer
+ * Processes an event lottery when called from the app by an organizer.
  */
 export const runLottery = onCall(async (request) => {
   const db = getFirestore();
@@ -29,8 +32,17 @@ export const runLottery = onCall(async (request) => {
       'The function must be called with a valid "eventId".'
     );
   }
-
-  if (!numberOfAttendees || typeof numberOfAttendees !== 'number') {
+  if (!organizerId || typeof organizerId !== 'string') {
+    throw new HttpsError(
+      'invalid-argument',
+      'The function must be called with a valid "organizerId".'
+    );
+  }
+  if (
+    !numberOfAttendees ||
+    typeof numberOfAttendees !== 'number' ||
+    numberOfAttendees < 1
+  ) {
     throw new HttpsError(
       'invalid-argument',
       'The function must be called with a valid "numberOfAttendees".'
@@ -64,6 +76,97 @@ export const runLottery = onCall(async (request) => {
     throw new HttpsError('internal', `An error has occurred: ${error}`);
   }
 });
+
+/**
+ * Task Queue Function: Processes the lottery for a given event after the deadline passes.
+ */
+export const runLotteryByQueue = onTaskDispatched(
+  {
+    retryConfig: {
+      maxAttempts: 3,
+      minBackoffSeconds: 60,
+    },
+    rateLimits: {
+      maxConcurrentDispatches: 3,
+    },
+  },
+  async (req) => {
+    const eventId = req.data.eventId;
+
+    if (!eventId) {
+      logger.error('No eventId provided for lottery processing');
+      throw new Error('Missing eventId');
+    }
+    logger.info(`Processing lottery for event: ${eventId}`);
+
+    try {
+      const snapshot = await db.collection('events').doc(eventId).get();
+
+      if (!snapshot.exists) {
+        logger.error(`Event with ID ${eventId} does not exist.`);
+        return;
+      }
+      const eventData = snapshot.data() as AppEvent;
+
+      if (!eventData) {
+        logger.error(`Event data is null for ID ${eventId}`);
+        return;
+      }
+      if (eventData.lotteryProcessed == true) {
+        logger.info(`Lottery already processed for event: ${eventId}.`);
+        return;
+      }
+
+      await processLottery(db, eventId, eventData, eventData.numberOfAttendees);
+
+      logger.debug(`Lottery processed successfully for event: ${eventId}`);
+    } catch (error) {
+      logger.error(`Error processing lottery for event ${eventId}:`, error);
+      throw error;
+    }
+  }
+);
+
+/**
+ * When an event is created
+ * Enroll it into the lottery system.
+ */
+export const handleEventCreated = onDocumentCreated(
+  'events/{eventId}',
+  async (event) => {
+    const snapshot = event.data;
+
+    if (!snapshot) {
+      logger.warn('handleEventCreated: No data found');
+      return;
+    }
+    const eventId = event.params.eventId;
+    const data = snapshot.data() as AppEvent;
+    const deadlineTimestamp = data.deadline;
+
+    logger.debug(`Enrolling event with id ${eventId} into the lottery system`);
+    const queue = getFunctions().taskQueue('processLottery');
+
+    try {
+      await queue.enqueue(
+        { eventId },
+        {
+          scheduleDelaySeconds: Math.max(
+            0,
+            Math.floor(deadlineTimestamp / 1000) - Math.floor(Date.now() / 1000)
+          ),
+        }
+      );
+
+      logger.info(`Lottery task scheduled for event: ${eventId}`);
+    } catch (error) {
+      logger.error(
+        `Failed to schedule lottery task for event ${eventId}:`,
+        error
+      );
+    }
+  }
+);
 
 /**
  * When a user removes their organizer status
@@ -129,6 +232,14 @@ export const handleUserDeleted = onDocumentDeleted(
       {
         name: 'signups',
         query: db.collection('signups').where('userId', '==', userId),
+      },
+      {
+        name: 'notifications',
+        query: db
+          .collection('users')
+          .doc(userId)
+          .collection('notifications')
+          .where('userId', '==', userId),
       },
     ];
 
--- functions/src/types/signup.ts
+++ functions/src/types/signup.ts
@@ -1,4 +1,8 @@
 export interface Signup {
   userId: string;
   eventId: boolean;
+  waitlisted: boolean;
+  cancelled: boolean;
+  chosen: boolean;
+  enrolled: boolean;
 }
