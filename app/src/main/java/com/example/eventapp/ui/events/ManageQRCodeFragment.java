2c70a54122c21652ddaab4075fe4e5e5acaeef3c
Yujin <146316031+yjulianlp@users.noreply.github.com>
2024-12-02 01:07:12 -0700

    feat: generate qr code image on demand and display it

--- app/src/main/java/com/example/eventapp/ui/events/ManageQRCodeFragment.java
+++ app/src/main/java/com/example/eventapp/ui/events/ManageQRCodeFragment.java
@@ -6,12 +6,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.eventapp.R;
 import com.example.eventapp.models.Event;
+import com.example.eventapp.services.QRCodeGenerator;
+import com.example.eventapp.viewmodels.EntrantsViewModel;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class ManageQRCodeFragment extends BottomSheetDialogFragment {
-    private final Event event;
+    private Event event;
+    private EntrantsViewModel entrantsViewModel;
 
     public ManageQRCodeFragment(Event event) {
         this.event = event;
@@ -21,9 +26,13 @@ public class ManageQRCodeFragment extends BottomSheetDialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_manage_qr_code, null);
         ImageView qrCodeImage = view.findViewById(R.id.fragment_manage_qr_code_qr_code_image);
+        entrantsViewModel = new ViewModelProvider(requireActivity()).get(EntrantsViewModel.class);
+        event = entrantsViewModel.getCurrentEventToQuery();
 
         // set image to the qr code image
-
+        QRCodeGenerator qrCodeGenerator = new QRCodeGenerator(event.getQrCodeHash(), 400, 400);
+        qrCodeGenerator.generateQRCodeBitmap();
+        qrCodeImage.setImageBitmap(qrCodeGenerator.getQrCodeBitmap());
 
         return view;
     }
